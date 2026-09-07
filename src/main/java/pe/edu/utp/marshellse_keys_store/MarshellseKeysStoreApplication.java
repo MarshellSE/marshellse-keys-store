b3ddef953c1a06d3d14db1b7ac9dd4ddad3c3f32
MarshellSE <marshell99.se@gmail.com>
2026-09-06 20:49:46 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/utp/marshellse_keys_store/MarshellseKeysStoreApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.marshellse_keys_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarshellseKeysStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarshellseKeysStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/utp/marshellse_keys_store/MarshellseKeysStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.marshellse_keys_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarshellseKeysStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
